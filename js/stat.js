896e4b93fe67a4214d09f8bd7c05aa7e6e797731
Selivanov <superexpulsion@mail.ru>
2018-04-02 18:39:39 +0300

    Переделал на свой лад

--- js/stat.js
+++ js/stat.js
@@ -1,42 +1,52 @@
-'use strict'; // Зачем его писать?
+'use strict';
 
-window.renderStatistics = function (ctx, names, times) { // зачем window
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 420, 270);
-  ctx.fillStyle = 'white';
-  ctx.fillRect(100, 10, 420, 270);
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура, вы победили!', 120, 30);
-  var max = -1;
-  for (var i = 0; i < times.length; i++) { // а могу ВАР перед И неписать? Зачем длину времени берем?
-    var time = times[i]; // почему значене как массив?
-    if (time > max) {
-      max = time;
+window.renderStatistics = function (ctx, names, times) {
+  var BAR_HEIGHT = 150;
+  var INDENT = 50;
+  var BAR_WIDTH = 40;
+  var INITIAL_X = 140;
+  var INITIAL_Y = 250;
+  var drawCloud = function (context) { // Рисуем облака
+    context.fillStyle = 'rgba(0, 0, 0, 0.7)';
+    context.fillRect(110, 20, 420, 270);
+    context.fillStyle = 'white';
+    context.fillRect(100, 10, 420, 270);
+    context.fillStyle = '#000';
+    context.font = '16px PT Mono';
+    context.textBaseline = 'hanging';
+    context.fillText('Ура, вы победили!', 120, 30);
+  };
+  var getMaxElement = function (array) { // Получаем максимального элемента - для времени
+    var max = -1;
+    for (var i = 0; i < array.length; i++) {
+      var time = array[i];
+      if (time > max) {
+        max = time;
+      }
     }
-  }
-  var histogramWidth = 150;
-  var step = histogramWidth / (max - 0); // почему так? что за шаг
-  ctx.fillText('Список результатов: ', 120, 50);
-  var barHeigth = 50;
-  var indent = 90;
-  var initialX = 250; // почему их задаем?
-  var initialY = 140; // почему их задаем?
-  ctx.textBaseline = 'hanging';
-  var getColor = function (name) { // name нигде не определ до этого , почему он в скобках появился?
+    return max;
+  };
+  var getColor = function (name) { // Получаем цвет для чела с именем name
     var color;
     if (name === 'Вы') { // объяснить значение трех равно и смыл скобок
       color = 'rgba(255, 0, 0, 1.0)';
     } else {
-      color = 'rgba(0, 0, 255, ' + Math.random() + ')'; // а если в скобках рендома что-то будет, что это будет значить?
+      color = 'rgba(0, 0, 255, ' + Math.random() + ')';
     }
     return color;
   };
-  for (i = 0; i < times.length; i++) { // объяснить содержание скобок
-    ctx.fillStyle = getColor(names[i]); // почему в скобках И?
-    ctx.fillRect(initialY + indent * i, initialX, barHeigth, -times[i] * step);
-    ctx.fillText(names[i], initialY + indent * i, initialX + 5);
-    ctx.fillText(times[i].toFixed(0), initialY + indent * i, initialX - times[i] * step - 20);
+  var drawResult = function (name, time, position, step) { // получаем результат одного чела
+    ctx.fillStyle = getColor(name);
+    ctx.fillRect(INITIAL_X + (INDENT + BAR_WIDTH) * position, INITIAL_Y, BAR_WIDTH, -time * step);
+    ctx.fillText(name, INITIAL_X + (INDENT + BAR_WIDTH) * position, INITIAL_Y + 5);
+    ctx.fillText(time.toFixed(0), INITIAL_X + (INDENT + BAR_WIDTH) * position, INITIAL_Y - time * step - 20);
+  };
+  var maxTime = getMaxElement(times);
+  var step = BAR_HEIGHT / maxTime;
+  drawCloud(ctx);
+  ctx.fillText('Список результатов: ', 120, 50);
+  ctx.textBaseline = 'hanging';
+  for (var i = 0; i < times.length; i++) {
+    drawResult(names[i], times[i], i, step);
   }
 };

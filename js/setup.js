f2f318b590d898a4329a9f22e5eeb50fb8c2704a
Selivanov <superexpulsion@mail.ru>
2018-04-20 23:42:32 +0300

    сделал перетаскивание окна путем смены местами тегов

--- js/setup.js
+++ js/setup.js
@@ -41,6 +41,9 @@ var setupOpen = document.querySelector('.setup-open');
 var setup = document.querySelector('.setup');
 var setupClose = setup.querySelector('.setup-close');
 
+var coordinatesSetupTop = setup.style.top;
+var coordinatesSetupLeft = setup.style.left;
+
 var ESC_KEYCODE = 27;
 var ENTER_KEYCODE = 13;
 
@@ -58,6 +61,8 @@ var openPopup = function () {
 var closePopup = function () {
   setup.classList.add('hidden');
   document.removeEventListener('keydown', onPopupEscPress);
+  setup.style.top = coordinatesSetupTop;
+  setup.style.left = coordinatesSetupLeft;
 };
 
 setupOpen.addEventListener('click', function () {
@@ -133,4 +138,43 @@ wizardFireball.addEventListener('click', function () {
   var colorFireball = COLOR_WIZARD_FIREBALL[getRandomIndex(0, COLOR_WIZARD_FIREBALL.length - 1)];
   wizardFireball.style.background = colorFireball;
   wizardFireballColor.value = colorFireball;
+});
+
+
+var dialogHandle = setup.querySelector('.setup-user-pic'); // находим элемент за который тащим
+
+dialogHandle.addEventListener('mousedown', function (evt) { // обработаем событие начала перетаскивания нашего диалога
+  evt.preventDefault();
+
+  var startCoords = { // Запомним координаты точки, с которой мы начали перемещать диалог
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    setup.style.top = (setup.offsetTop - shift.y) + 'px';
+    setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove); // Добавим обработчики события передвижения мыши
+  document.addEventListener('mouseup', onMouseUp); // Добавим обработчики события отпускания кнопки мыши
 });
